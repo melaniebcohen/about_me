6bb2a4c41e30d2e1085eb546287ed2c73c094a1c
Melanie Cohen <melaniebcohen@gmail.com>
2017-12-06 09:33:27 -0800

    adding y/n statements

--- app.js
+++ app.js
@@ -10,9 +10,9 @@ var userHomeYorN = prompt('Yes or No: I am from Seattle, Washington.');
 userHomeYorN = userHomeYorN.toUpperCase();
 console.log('Am I from Seattle? User answered:',userHomeYorN);
 
-if (userHomeYorN === 'YES') {
+if (userHomeYorN === 'YES' || userHomeYorN === 'Y') {
   alert('That\'s correct, ' + userName + '. I am from Seattle.');
-} else if (userHomeYorN === 'NO') {
+} else if (userHomeYorN === 'NO' || userHomeYorN === 'N') {
   alert('Not quite, ' + userName + '. I am indeed from Seattle.');
 };
 
@@ -21,9 +21,9 @@ var userExercise = prompt('Yes or No: Running is my favorite form of exercise.')
 userExercise = userExercise.toUpperCase();
 console.log('Is running my favorite form of exercise? User answered: ' + userExercise);
 
-if (userExercise === 'YES') {
+if (userExercise === 'YES' || userExercise === 'Y') {
   alert('Not quite! My favorite form of exercise is actually weightlifting.');
-} else if (userExercise === 'NO') {
+} else if (userExercise === 'NO' || userExercise === 'N') {
   alert('Correct, my favorite form of exercise is actually weightlifting rather than running.');
 };
 
@@ -32,9 +32,9 @@ var userKids = prompt('Yes or No: I have kids.');
 userKids = userKids.toUpperCase();
 console.log('Do I have kids? User answered:',userKids);
 
-if (userKids === 'YES') {
+if (userKids === 'YES' || userKids === 'Y') {
   alert('Wrong! Kids are great, but I have a niece and nephew to keep me entertained instead.');
-} else if (userKids === 'NO') {
+} else if (userKids === 'NO' || userKids === 'N') {
   alert('Correct, no kids for me. I\'m good.');
 };
 
@@ -43,9 +43,9 @@ var userHasCats = prompt('Yes or No: I have cats at home.');
 userHasCats = userHasCats.toUpperCase();
 console.log('Does user have two cats? User answered:',userHasCats);
 
-if (userHasCats === 'YES') {
+if (userHasCats === 'YES' || userHasCats === 'Y') {
   alert('Yes, ' + userName + ', I do have two cats. I promise I try not to be a crazy cat lady most of the time.');
-} else if (userHasCats === 'NO') {
+} else if (userHasCats === 'NO' || userHasCats === 'N') {
   alert('Wrong! I adopted two cats last year.');
 };
 
@@ -54,8 +54,8 @@ var userCareVolunteer = prompt('Yes or No: I volunteer virtually for a exotic an
 userCareVolunteer = userCareVolunteer.toUpperCase();
 console.log('Do I volunteer for CARE? User answered:',userCareVolunteer);
 
-if (userCareVolunteer === 'YES') {
+if (userCareVolunteer === 'YES' || userCareVolunteer === 'Y') {
   alert('Yes, I volunteer virtually for an animal sanctuary called CARE Rescue Texas. Although my volunteer days are probably over for a few months now.');
-} else if (userCareVolunteer === 'NO') {
+} else if (userCareVolunteer === 'NO' || userCareVolunteer === 'N') {
   alert('False! I volunteer virtually for an animal sanctuary called CARE Rescue Texas. Random, I know.');
 };
